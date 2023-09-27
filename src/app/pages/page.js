b7e8aed527e4f572a2ec8032457e066dd33ef546
DinaX <dinisantunes@ua.pt>
2023-09-27 16:53:50 +0100

    link to new page

---
+++ src/app/pages/page.js
@@ -0,0 +1,14 @@
+import "../newstyles.css"
+import Link from "next/link"
+export default function Hello() {
+    return (
+
+ <div> 
+    
+    <h1>Oláa!!!</h1>
+ <Link href="/"><button>Return Home :)</button></Link>
+ </div>
+
+    )
+    
+  }
