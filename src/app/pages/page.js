c2829639b9ca93d4aa9c3a35f55e13dba7e93489
DinaX <dinisantunes@ua.pt>
2023-10-09 19:21:57 +0100

    try again

--- src/app/pages/page.js
+++ src/app/pages/page.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Hello() {
   return (
     <div>
-      <h1 className="h1_ola">Oláa!!!</h1>
+      <h1 className="h1_ola">Oláa!!</h1>
       <Link href="/">
         <button>Return Home :)</button>
       </Link>
